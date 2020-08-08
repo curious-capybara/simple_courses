cdd4fc494703e9c4157c989cef5845ddbf52d8ac
Paweł Świątkowski <katafrakt@vivaldi.net>
2020-08-08 23:04:05 +0200

    Refactor user domain using result monad

--- app/domains/user_domain.rb
+++ app/domains/user_domain.rb
@@ -1,15 +1,36 @@
+# frozen_string_literal: true
+
+require 'dry/monads'
+
 module UserDomain
+  extend Dry::Monads[:result]
+
   # Creates a new user
   #
   # @param email [String] email of a user to be created, must be unique
   def self.create_user(email:)
-    User.create(email: email)
+    user = User.new(email: email)
+    if user.save
+      Success(user)
+    else
+      Failure([:failed, user.errors.messages])
+    end
+  rescue StandardError => e
+    Failure([:error, e])
   end
 
   # Deletes a user by their id
   #
   # @param id [Integer] user id
   def self.delete_user(id:)
-    User.find_by(id: id)&.destroy
+    user = User.find_by(id: id)
+    if user
+      user.destroy
+      Success(user)
+    else
+      Failure([:failed, { base: ['user does not exist'] }])
+    end
+  rescue StandardError => e
+    Failure([:error, e])
   end
 end
--- spec/unit/domains/user_domain_spec.rb
+++ spec/unit/domains/user_domain_spec.rb
@@ -3,36 +3,85 @@
 require 'rails_helper'
 
 RSpec.describe UserDomain do
+  include Dry::Monads[:result]
+
   let(:email) { 'test@example.com' }
 
   describe '#create_user' do
-    it 'creates a new user' do
-      expect { UserDomain.create_user(email: email) }.to change { User.count }.by(1)
+    context 'with correct params' do
+      let(:result) { UserDomain.create_user(email: email) }
+
+      it 'creates a new user' do
+        expect { result }.to change { User.count }.by(1)
+      end
+
+      it 'returns Success' do
+        expect(result).to be_success
+        expect(result.value!).to be_kind_of(User)
+        expect(result.value!.email).to eq(email)
+      end
     end
 
-    it 'does not create a user with a duplicate email' do
-      User.create!(email: email)
-      expect { UserDomain.create_user(email: email) }.not_to change { User.count }
+    context 'with duplicated email' do
+      before { User.create!(email: email) }
+      let(:result) { UserDomain.create_user(email: email) }
+
+      it 'does not create a user with a duplicate email' do
+        expect { result }.not_to change { User.count }
+      end
+
+      it 'returns failure' do
+        expect(result).to be_failure
+        expect(result.failure).to eq([:failed, { email: ['has already been taken'] }])
+      end
     end
 
-    it 'does not create a user with empty email' do
-      expect { UserDomain.create_user(email: nil) }.not_to change { User.count }
+    context 'with empty email' do
+      let(:result) { UserDomain.create_user(email: nil) }
+
+      it 'does not create a user with empty email' do
+        expect { result }.not_to change { User.count }
+      end
+
+      it 'returns failure' do
+        expect(result).to be_failure
+        expect(result.failure).to eq([:failed, { email: ["can't be blank"] }])
+      end
     end
   end
 
   describe '#delete_user' do
-    let!(:user) { User.create!(email: email) }
+    context 'when user exists' do
+      let!(:user) { User.create!(email: email) }
+      let(:result) { UserDomain.delete_user(id: user.id) }
 
-    it 'deletes a user' do
-      expect { UserDomain.delete_user(id: user.id) }.to change { User.count }.by(-1)
+      it 'deletes a user' do
+        expect { result }.to change { User.count }.by(-1)
+      end
+
+      it 'does not delete other users' do
+        other_email = 'test2@example.com'
+        User.create!(email: other_email)
+        result
+        expect(User.find_by(email: other_email)).not_to eq(nil)
+        expect(User.find_by(email: email)).to eq(nil)
+      end
+
+      it 'returns success' do
+        expect(result).to be_success
+      end
     end
 
-    it 'does not delete other users' do
-      other_email = 'test2@example.com'
-      User.create!(email: other_email)
-      UserDomain.delete_user(id: user.id)
-      expect(User.find_by(email: other_email)).not_to eq(nil)
-      expect(User.find_by(email: email)).to eq(nil)
+    context 'when user does not exist' do
+      let(:result) do
+        last_id = User.order(:id).last || 0
+        UserDomain.delete_user(id: last_id + 1)
+      end
+
+      it 'returns failure' do
+        expect(result).to be_failure
+        expect(result.failure).to eq([:failed, { base: ['user does not exist'] }])
+      end
     end
   end
 end
